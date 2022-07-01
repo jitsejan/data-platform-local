529b70823752b691e6a0a873244ddd7d69da413a
Jitse-Jan <jitsejan@gmail.com>
2022-07-01 20:09:11 +0100

    Added repr to model

--- samples/sqlmodel_example.py
+++ samples/sqlmodel_example.py
@@ -1,22 +1,30 @@
+from typing import List
 
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 
 class Character(SQLModel, table=True):
+    """ Defines the character class """
     name: str = Field(primary_key=True)
     description: str
-
+    # Set the schema (if not default)
     __table_args__ = {'schema': 'smb_one'}
 
+    def __repr__(self):
+        return f"<Character(name='{self.name}', description='{self.description}')>"
 
+
+# Create the Postgres engine to connect to the database running in Docker
 engine = create_engine('postgresql://postgresuser:postgrespass@localhost:5423/nintendo')
 
 
-def select_characters():
+def get_characters() -> List[Character]:
+    """ Get all characters from the database """
     with Session(engine) as session:
         statement = select(Character)
-        results = session.exec(statement)
-        for character in results:
-            print(character)
+        return [char for char in session.exec(statement)]
 
-select_characters()
+
+# Retrieve and print the characters
+characters = get_characters()
+print(characters)
